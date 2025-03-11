bdaf25fc0036049e66e7c88a4d469bd340832291
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2025-03-11 12:11:39 +0000

    use biome explicitly

--- src/get-posts.ts
+++ src/get-posts.ts
@@ -1,25 +1,25 @@
 type ViteMarkdownGlobValues = {
-    url: string
-    rawContent: () => string;
-}
+	url: string;
+	rawContent: () => string;
+};
 
 type Post = {
-    url: string
-    heading: string
-}
+	url: string;
+	heading: string;
+};
 
 function getTitleFromRawContent(content: string) {
-    const firstLine = content.split("\n")[0]
-    return firstLine.split("# ")[1]
+	const firstLine = content.split('\n')[0];
+	return firstLine.split('# ')[1];
 }
 
 export function getAllPosts(): Post[] {
-    const allPosts = Object.values(
-        import.meta.glob('./pages/posts/*.md', { eager: true })
-    ) as ViteMarkdownGlobValues[];
+	const allPosts = Object.values(
+		import.meta.glob('./pages/posts/*.md', { eager: true }),
+	) as ViteMarkdownGlobValues[];
 
-    return allPosts.map(p => ({
-        url: p.url,
-        heading: getTitleFromRawContent(p.rawContent())
-    }))
+	return allPosts.map((p) => ({
+		url: p.url,
+		heading: getTitleFromRawContent(p.rawContent()),
+	}));
 }
